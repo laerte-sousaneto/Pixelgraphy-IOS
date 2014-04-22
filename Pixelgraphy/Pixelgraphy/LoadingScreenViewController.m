421d6adcbf3f812bf7723eeb87e675975915787d
anthony <a.paveglio@me.com>
2014-04-22 00:08:25 -0400

    Working on cross view controller communications

---
+++ Pixelgraphy/Pixelgraphy/LoadingScreenViewController.m
@@ -0,0 +1,38 @@
+//
+//  LoadingScreenViewController.m
+//  Pixelgraphy
+//
+//  Created by ODESSA on 4/21/14.
+//  Copyright (c) 2014 Laerte Sousa Neto. All rights reserved.
+//
+
+#import "LoadingScreenViewController.h"
+
+@interface LoadingScreenViewController ()
+
+@end
+
+@implementation LoadingScreenViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
