1754a1d55a652d0d5b53a398828ffbb011d93b06
anthony <a.paveglio@me.com>
2014-04-23 14:47:36 -0400

    Added some reference outlets to the profile view controller h file

--- Pixelgraphy/Pixelgraphy/ProfileViewController.h
+++ Pixelgraphy/Pixelgraphy/ProfileViewController.h
@@ -10,5 +10,13 @@
 
 @interface ProfileViewController : UIViewController
 @property (weak, nonatomic) IBOutlet UIScrollView *ScrollViewRO;
+@property (weak, nonatomic) IBOutlet UIImageView *ProfileImage;
+@property (weak, nonatomic) IBOutlet UILabel *Username;
+@property (weak, nonatomic) IBOutlet UILabel *Majors;
+@property (weak, nonatomic) IBOutlet UILabel *email;
+@property (weak, nonatomic) IBOutlet UILabel *dob;
+@property (weak, nonatomic) IBOutlet UILabel *Hometown;
+@property (weak, nonatomic) IBOutlet UITextView *HobbiesTextView;
+@property (weak, nonatomic) IBOutlet UITextView *BioTextView;
 
 @end
