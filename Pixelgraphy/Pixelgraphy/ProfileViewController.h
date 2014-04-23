9a48df8ab25e053bdfa8e522325697a8f709ec86
anthony <a.paveglio@me.com>
2014-04-23 13:55:35 -0400

    Added class file for profile Added some visual stuff to profile

---
+++ Pixelgraphy/Pixelgraphy/ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.h
+//  Pixelgraphy
+//
+//  Created by ODESSA on 4/23/14.
+//  Copyright (c) 2014 Laerte Sousa Neto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileViewController : UIViewController
+
+@end
