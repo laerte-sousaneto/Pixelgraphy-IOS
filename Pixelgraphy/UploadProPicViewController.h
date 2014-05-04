d00908ddfef4630162383ac338f388ec05bcc591
Skylar Gifford <sgiffio@me.com>
2014-05-04 01:29:15 -0400

    more graphic changes to storyboard

---
+++ Pixelgraphy/UploadProPicViewController.h
@@ -0,0 +1,20 @@
+//
+//  UploadProPicViewController.h
+//  Pixelgraphy
+//
+//  Created by Skylar Gifford on 5/4/14.
+//  Copyright (c) 2014 Laerte Sousa Neto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UploadProPicViewController : UIViewController
+
+
+
+
+
+@end
+
+
+
