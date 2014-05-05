519034ab93adf4888eff9473bad1a2502d5c57b9
Skylar Gifford <sgiffio@me.com>
2014-05-05 11:03:21 -0400

    added more views

--- Pixelgraphy/EditProfileViewController.m
+++ Pixelgraphy/EditProfileViewController.m
@@ -1,27 +1,32 @@
 //
-//  UploadProPicViewController.m
+//  EditProfileViewController.m
 //  Pixelgraphy
 //
-//  Created by Skylar Gifford on 5/4/14.
+//  Created by Skylar Gifford on 5/5/14.
 //  Copyright (c) 2014 Laerte Sousa Neto. All rights reserved.
 //
 
-#import "UploadProPicViewController.h"
+#import "EditProfileViewController.h"
 
-@interface UploadProPicViewController ()
+@interface EditProfileViewController ()
 
 @end
 
-@implementation UploadProPicViewController
+@implementation EditProfileViewController
 
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    
-    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismissKeyboard)];
-    [self.view addGestureRecognizer:tap];
 }
 
 - (void)didReceiveMemoryWarning
@@ -41,15 +46,4 @@
 }
 */
 
-- (IBAction)AddPhoto:(UIButton *)sender {
-}
-
-- (IBAction)UploadPhoto:(UIButton *)sender {
-}
-
--(void)dismissKeyboard
-{
-    [_PicName resignFirstResponder];
-    [_ImageDescript resignFirstResponder];
-}
 @end
