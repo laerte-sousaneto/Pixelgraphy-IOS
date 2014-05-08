82416f0998daf16fcecf8d9d028f33979adb4921
Skylar Gifford <sgiffio@me.com>
2014-05-07 20:31:13 -0400

    contactus view controller

---
+++ Pixelgraphy/ContactUsViewController.m
@@ -0,0 +1,33 @@
+//
+//  ContactUsViewController.m
+//  Pixelgraphy
+//
+//  Created by Skylar Gifford on 5/7/14.
+//  Copyright (c) 2014 Laerte Sousa Neto. All rights reserved.
+//
+
+#import "ContactUsViewController.h"
+
+@interface ContactUsViewController ()
+
+@end
+
+@implementation ContactUsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+}
+
+
+@end
